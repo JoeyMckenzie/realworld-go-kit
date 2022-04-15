f6cda861d2988b4e7cababf9e293a9b298ae4b3b
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-04-15 11:50:36 -0700

    chore(workspace): code cleanup

--- internal/articles/core/middlewares/articles_service_logging.go
+++ internal/articles/core/middlewares/articles_service_logging.go
@@ -1,97 +1,97 @@
 package middlewares
 
 import (
-    "context"
-    "fmt"
-    "github.com/go-kit/log"
-    "github.com/go-kit/log/level"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
-    "time"
+	"context"
+	"fmt"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
+	"time"
 )
 
 type articlesServiceLoggingMiddleware struct {
-    logger log.Logger
-    next   core.ArticlesService
+	logger log.Logger
+	next   core.ArticlesService
 }
 
 func NewArticlesServiceLoggingMiddleware(logger log.Logger) core.ArticlesServiceMiddleware {
-    return func(next core.ArticlesService) core.ArticlesService {
-        return &articlesServiceLoggingMiddleware{
-            logger: logger,
-            next:   next,
-        }
-    }
+	return func(next core.ArticlesService) core.ArticlesService {
+		return &articlesServiceLoggingMiddleware{
+			logger: logger,
+			next:   next,
+		}
+	}
 }
 
 func (mw *articlesServiceLoggingMiddleware) GetArticles(ctx context.Context, request *domain.GetArticlesServiceRequest) (articles *[]domain.ArticleDto, err error) {
-    defer func(begin time.Time) {
-        level.Info(mw.logger).Log(
-            "method", "CreateArticle",
-            "request_time", time.Since(begin),
-            "articles_found", fmt.Sprint(articles != nil),
-            "error", err,
-        )
-    }(time.Now())
+	defer func(begin time.Time) {
+		level.Info(mw.logger).Log(
+			"method", "CreateArticle",
+			"request_time", time.Since(begin),
+			"articles_found", fmt.Sprint(articles != nil),
+			"error", err,
+		)
+	}(time.Now())
 
-    level.Info(mw.logger).Log(
-        "method", "GetArticles",
-        "request", request.ToSafeLoggingStruct(),
-    )
+	level.Info(mw.logger).Log(
+		"method", "GetArticles",
+		"request", request.ToSafeLoggingStruct(),
+	)
 
-    return mw.next.GetArticles(ctx, request)
+	return mw.next.GetArticles(ctx, request)
 }
 
 func (mw *articlesServiceLoggingMiddleware) GetArticle(ctx context.Context, request *domain.GetArticleServiceRequest) (article *domain.ArticleDto, err error) {
-    defer func(begin time.Time) {
-        level.Info(mw.logger).Log(
-            "method", "GetArticle",
-            "request_time", time.Since(begin),
-            "article_found", fmt.Sprint(article != nil),
-            "error", err,
-        )
-    }(time.Now())
+	defer func(begin time.Time) {
+		level.Info(mw.logger).Log(
+			"method", "GetArticle",
+			"request_time", time.Since(begin),
+			"article_found", fmt.Sprint(article != nil),
+			"error", err,
+		)
+	}(time.Now())
 
-    level.Info(mw.logger).Log(
-        "method", "GetArticle",
-        "slug", request.Slug,
-        "user_id", request.UserId,
-    )
+	level.Info(mw.logger).Log(
+		"method", "GetArticle",
+		"slug", request.Slug,
+		"user_id", request.UserId,
+	)
 
-    return mw.next.GetArticle(ctx, request)
+	return mw.next.GetArticle(ctx, request)
 }
 
 func (mw *articlesServiceLoggingMiddleware) CreateArticle(ctx context.Context, request *domain.UpsertArticleServiceRequest) (article *domain.ArticleDto, err error) {
-    defer func(begin time.Time) {
-        level.Info(mw.logger).Log(
-            "method", "CreateArticle",
-            "request_time", time.Since(begin),
-            "error", err,
-        )
-    }(time.Now())
+	defer func(begin time.Time) {
+		level.Info(mw.logger).Log(
+			"method", "CreateArticle",
+			"request_time", time.Since(begin),
+			"error", err,
+		)
+	}(time.Now())
 
-    level.Info(mw.logger).Log(
-        "method", "CreateArticle",
-        "request", request.ToSafeLoggingStruct(),
-    )
+	level.Info(mw.logger).Log(
+		"method", "CreateArticle",
+		"request", request.ToSafeLoggingStruct(),
+	)
 
-    return mw.next.CreateArticle(ctx, request)
+	return mw.next.CreateArticle(ctx, request)
 }
 
 func (mw *articlesServiceLoggingMiddleware) GetFeed(ctx context.Context, request *domain.GetArticlesServiceRequest) (articles *[]domain.ArticleDto, err error) {
-    defer func(begin time.Time) {
-        level.Info(mw.logger).Log(
-            "method", "GetFeed",
-            "request_time", time.Since(begin),
-            "articles_found", fmt.Sprint(articles != nil),
-            "error", err,
-        )
-    }(time.Now())
+	defer func(begin time.Time) {
+		level.Info(mw.logger).Log(
+			"method", "GetFeed",
+			"request_time", time.Since(begin),
+			"articles_found", fmt.Sprint(articles != nil),
+			"error", err,
+		)
+	}(time.Now())
 
-    level.Info(mw.logger).Log(
-        "method", "GetFeed",
-        "request", request.ToSafeLoggingStruct(),
-    )
+	level.Info(mw.logger).Log(
+		"method", "GetFeed",
+		"request", request.ToSafeLoggingStruct(),
+	)
 
-    return mw.next.GetFeed(ctx, request)
+	return mw.next.GetFeed(ctx, request)
 }
--- internal/articles/core/middlewares/articles_service_validator.go
+++ internal/articles/core/middlewares/articles_service_validator.go
@@ -1,59 +1,59 @@
 package middlewares
 
 import (
-    "context"
-    "github.com/go-kit/log"
-    "github.com/go-playground/validator/v10"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
-    "github.com/joeymckenzie/realworld-go-kit/pkg/api"
-    "github.com/joeymckenzie/realworld-go-kit/pkg/utilities"
+	"context"
+	"github.com/go-kit/log"
+	"github.com/go-playground/validator/v10"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
+	"github.com/joeymckenzie/realworld-go-kit/pkg/api"
+	"github.com/joeymckenzie/realworld-go-kit/pkg/utilities"
 )
 
 type articlesServiceRequestValidationMiddleware struct {
-    logger    log.Logger
-    validator *validator.Validate
-    next      core.ArticlesService
+	logger    log.Logger
+	validator *validator.Validate
+	next      core.ArticlesService
 }
 
 func NewArticlesServiceRequestValidationMiddleware(logger log.Logger, validator *validator.Validate) core.ArticlesServiceMiddleware {
-    return func(next core.ArticlesService) core.ArticlesService {
-        return &articlesServiceRequestValidationMiddleware{
-            logger:    logger,
-            validator: validator,
-            next:      next,
-        }
-    }
+	return func(next core.ArticlesService) core.ArticlesService {
+		return &articlesServiceRequestValidationMiddleware{
+			logger:    logger,
+			validator: validator,
+			next:      next,
+		}
+	}
 }
 
 func (mw *articlesServiceRequestValidationMiddleware) GetArticles(ctx context.Context, request *domain.GetArticlesServiceRequest) (*[]domain.ArticleDto, error) {
-    if request == nil {
-        return nil, utilities.ErrNilInput
-    }
+	if request == nil {
+		return nil, utilities.ErrNilInput
+	}
 
-    return mw.next.GetArticles(ctx, request)
+	return mw.next.GetArticles(ctx, request)
 }
 
 func (mw *articlesServiceRequestValidationMiddleware) GetArticle(ctx context.Context, request *domain.GetArticleServiceRequest) (*domain.ArticleDto, error) {
-    if request == nil {
-        return nil, utilities.ErrNilInput
-    }
+	if request == nil {
+		return nil, utilities.ErrNilInput
+	}
 
-    return mw.next.GetArticle(ctx, request)
+	return mw.next.GetArticle(ctx, request)
 }
 
 func (mw *articlesServiceRequestValidationMiddleware) CreateArticle(ctx context.Context, request *domain.UpsertArticleServiceRequest) (*domain.ArticleDto, error) {
-    if err := mw.validator.Struct(request); err != nil {
-        return nil, api.NewValidationError(err)
-    }
+	if err := mw.validator.Struct(request); err != nil {
+		return nil, api.NewValidationError(err)
+	}
 
-    return mw.next.CreateArticle(ctx, request)
+	return mw.next.CreateArticle(ctx, request)
 }
 
 func (mw *articlesServiceRequestValidationMiddleware) GetFeed(ctx context.Context, request *domain.GetArticlesServiceRequest) (*[]domain.ArticleDto, error) {
-    if request == nil {
-        return nil, utilities.ErrNilInput
-    }
+	if request == nil {
+		return nil, utilities.ErrNilInput
+	}
 
-    return mw.next.GetFeed(ctx, request)
+	return mw.next.GetFeed(ctx, request)
 }
