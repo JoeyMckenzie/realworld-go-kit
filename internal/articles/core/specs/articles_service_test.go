5f737e4b69a964fc2d991d04763c7d9d41d19ade
Joey McKenzie <joey.mckenzie27@gmail.com>
2022-04-19 19:09:14 -0700

    chore(workspace): code cleanup

--- internal/articles/core/specs/articles_service_test.go
+++ internal/articles/core/specs/articles_service_test.go
@@ -1,47 +1,47 @@
 package specs
 
 import (
-    "context"
-    "entgo.io/ent/dialect"
-    "github.com/joeymckenzie/realworld-go-kit/ent/enttest"
-    "github.com/joeymckenzie/realworld-go-kit/internal"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
-    articlesPersistence "github.com/joeymckenzie/realworld-go-kit/internal/articles/persistence"
-    "testing"
+	"context"
+	"entgo.io/ent/dialect"
+	"github.com/joeymckenzie/realworld-go-kit/ent/enttest"
+	"github.com/joeymckenzie/realworld-go-kit/internal"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/core"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
+	articlesPersistence "github.com/joeymckenzie/realworld-go-kit/internal/articles/persistence"
+	"testing"
 )
 
 var (
-    StubCreateArticleRequest = domain.UpsertArticleServiceRequest{
-        UserId:      1,
-        Title:       "stub title",
-        Description: "stub description",
-        Body:        "stub body",
-        TagList:     &[]string{"stub tag"},
-    }
-    StubCreateArticleRequestWithoutTagList = domain.UpsertArticleServiceRequest{
-        UserId:      1,
-        Title:       "stub title",
-        Description: "stub description",
-        Body:        "stub body",
-    }
+	StubCreateArticleRequest = domain.UpsertArticleServiceRequest{
+		UserId:      1,
+		Title:       "stub title",
+		Description: "stub description",
+		Body:        "stub body",
+		TagList:     &[]string{"stub tag"},
+	}
+	StubCreateArticleRequestWithoutTagList = domain.UpsertArticleServiceRequest{
+		UserId:      1,
+		Title:       "stub title",
+		Description: "stub description",
+		Body:        "stub body",
+	}
 )
 
 type articlesServiceTestFixture struct {
-    mockArticlesRepository *articlesPersistence.MockArticlesRepository
-    service                core.ArticlesService
-    ctx                    context.Context
+	mockArticlesRepository *articlesPersistence.MockArticlesRepository
+	service                core.ArticlesService
+	ctx                    context.Context
 }
 
 func newArticlesServiceTestFixture(t *testing.T) *articlesServiceTestFixture {
-    mockArticlesRepository := new(articlesPersistence.MockArticlesRepository)
-    ctx := context.Background()
-    testClient := enttest.Open(t, dialect.SQLite, "file:realworld_go_kit?mode=memory&cache=shared&_fk=1")
-    internal.SeedData(ctx, testClient)
+	mockArticlesRepository := new(articlesPersistence.MockArticlesRepository)
+	ctx := context.Background()
+	testClient := enttest.Open(t, dialect.SQLite, "file:realworld_go_kit?mode=memory&cache=shared&_fk=1")
+	internal.SeedData(ctx, testClient)
 
-    return &articlesServiceTestFixture{
-        mockArticlesRepository: mockArticlesRepository,
-        service:                core.NewArticlesServices(nil, testClient),
-        ctx:                    context.Background(),
-    }
+	return &articlesServiceTestFixture{
+		mockArticlesRepository: mockArticlesRepository,
+		service:                core.NewArticlesServices(nil, testClient),
+		ctx:                    context.Background(),
+	}
 }
--- internal/articles/core/specs/create_article_test.go
+++ internal/articles/core/specs/create_article_test.go
@@ -1,33 +1,33 @@
 package specs
 
 import (
-    "github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
-    articlesPersistence "github.com/joeymckenzie/realworld-go-kit/internal/articles/persistence"
-    "github.com/stretchr/testify/assert"
-    "github.com/stretchr/testify/mock"
-    "testing"
+	"github.com/joeymckenzie/realworld-go-kit/internal/articles/domain"
+	articlesPersistence "github.com/joeymckenzie/realworld-go-kit/internal/articles/persistence"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
 )
 
 var (
-    createArticleRequestStub = &domain.UpsertArticleServiceRequest{
-        UserId:      1,
-        Title:       "stub title",
-        Description: "stub description",
-        Body:        "stub body",
-        TagList:     &[]string{"stub tag"},
-    }
+	createArticleRequestStub = &domain.UpsertArticleServiceRequest{
+		UserId:      1,
+		Title:       "stub title",
+		Description: "stub description",
+		Body:        "stub body",
+		TagList:     &[]string{"stub tag"},
+	}
 
-    createArticleRequestStubWithoutTagList = &domain.UpsertArticleServiceRequest{
-        UserId:      1,
-        Title:       "stub title",
-        Description: "stub description",
-        Body:        "stub body",
-    }
+	createArticleRequestStubWithoutTagList = &domain.UpsertArticleServiceRequest{
+		UserId:      1,
+		Title:       "stub title",
+		Description: "stub description",
+		Body:        "stub body",
+	}
 )
 
 func TestArticlesService(t *testing.T) {
-    t.Parallel()
-    t.Run("testFuck", testFuck)
+	t.Parallel()
+	t.Run("testFuck", testFuck)
 }
 
 func testFuck(t *testing.T) {
@@ -35,69 +35,69 @@ func testFuck(t *testing.T) {
 }
 
 func Test_CreateArticle_GivenValidRequestWithoutExistingTags_ReturnsSuccessfulResponseAndInsertsNewTags(t *testing.T) {
-    // Arrange
-    fixture := newArticlesServiceTestFixture(t)
+	// Arrange
+	fixture := newArticlesServiceTestFixture(t)
 
-    fixture.mockArticlesRepository.
-        On("FindArticleBySlug", fixture.ctx, mock.AnythingOfType("string")).
-        Return(nil, nil)
+	fixture.mockArticlesRepository.
+		On("FindArticleBySlug", fixture.ctx, mock.AnythingOfType("string")).
+		Return(nil, nil)
 
-    fixture.mockArticlesRepository.
-        On("GetTags", fixture.ctx, mock.AnythingOfType("[]string")).
-        Return(nil, nil)
+	fixture.mockArticlesRepository.
+		On("GetTags", fixture.ctx, mock.AnythingOfType("[]string")).
+		Return(nil, nil)
 
-    fixture.mockArticlesRepository.
-        On("CreateTag", fixture.ctx, mock.AnythingOfType("string")).
-        Return(articlesPersistence.StubTag, nil)
+	fixture.mockArticlesRepository.
+		On("CreateTag", fixture.ctx, mock.AnythingOfType("string")).
+		Return(articlesPersistence.StubTag, nil)
 
-    fixture.mockArticlesRepository.
-        On("CreateArticle", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).
-        Return(articlesPersistence.StubArticle, nil)
+	fixture.mockArticlesRepository.
+		On("CreateArticle", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).
+		Return(articlesPersistence.StubArticle, nil)
 
-    fixture.mockArticlesRepository.
-        On("CreateArticleTag", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("int")).
-        Return(articlesPersistence.StubArticleTag, nil)
+	fixture.mockArticlesRepository.
+		On("CreateArticleTag", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("int")).
+		Return(articlesPersistence.StubArticleTag, nil)
 
-    // Act
-    response, err := fixture.service.CreateArticle(fixture.ctx, createArticleRequestStub)
+	// Act
+	response, err := fixture.service.CreateArticle(fixture.ctx, createArticleRequestStub)
 
-    // Assert
-    assert.NotNil(t, response)
-    assert.Nil(t, err)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "FindArticleBySlug", 1)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "GetTags", 1)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateTag", 1)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateArticleTag", 1)
+	// Assert
+	assert.NotNil(t, response)
+	assert.Nil(t, err)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "FindArticleBySlug", 1)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "GetTags", 1)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateTag", 1)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateArticleTag", 1)
 }
 
 func Test_CreateArticle_GivenValidRequestWithoutTags_ReturnsSuccessfulResponseWithoutCreatingTag(t *testing.T) {
-    // Arrange
-    fixture := newArticlesServiceTestFixture(t)
+	// Arrange
+	fixture := newArticlesServiceTestFixture(t)
 
-    fixture.mockArticlesRepository.
-        On("FindArticleBySlug", fixture.ctx, mock.AnythingOfType("string")).
-        Return(nil, nil)
+	fixture.mockArticlesRepository.
+		On("FindArticleBySlug", fixture.ctx, mock.AnythingOfType("string")).
+		Return(nil, nil)
 
-    fixture.mockArticlesRepository.
-        On("GetTags", fixture.ctx, mock.AnythingOfType("[]string")).
-        Return(nil, nil)
+	fixture.mockArticlesRepository.
+		On("GetTags", fixture.ctx, mock.AnythingOfType("[]string")).
+		Return(nil, nil)
 
-    fixture.mockArticlesRepository.
-        On("CreateArticle", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).
-        Return(articlesPersistence.StubArticle, nil)
+	fixture.mockArticlesRepository.
+		On("CreateArticle", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string"), mock.AnythingOfType("string")).
+		Return(articlesPersistence.StubArticle, nil)
 
-    fixture.mockArticlesRepository.
-        On("CreateArticleTag", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("int")).
-        Return(articlesPersistence.StubArticleTag, nil)
+	fixture.mockArticlesRepository.
+		On("CreateArticleTag", fixture.ctx, mock.AnythingOfType("int"), mock.AnythingOfType("int")).
+		Return(articlesPersistence.StubArticleTag, nil)
 
-    // Act
-    response, err := fixture.service.CreateArticle(fixture.ctx, createArticleRequestStubWithoutTagList)
+	// Act
+	response, err := fixture.service.CreateArticle(fixture.ctx, createArticleRequestStubWithoutTagList)
 
-    // Assert
-    assert.NotNil(t, response)
-    assert.Nil(t, err)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "FindArticleBySlug", 1)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "GetTags", 0)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateTag", 0)
-    fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateArticleTag", 0)
+	// Assert
+	assert.NotNil(t, response)
+	assert.Nil(t, err)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "FindArticleBySlug", 1)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "GetTags", 0)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateTag", 0)
+	fixture.mockArticlesRepository.AssertNumberOfCalls(t, "CreateArticleTag", 0)
 }
